3c4c725ba6c49e6c167c27223ab309c4f3203e9a
OႦʂϙ <63944348+obsq@users.noreply.github.com>
2020-05-20 14:07:48 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,19 +1,14 @@
 class Translation(object):
-    START_TEXT = """`Hey I'am kyneRename Bot!`
+    START_TEXT = """`Hey I'am kyneRename Bot!`\n`I'am a file Renamer Bot!`\n\n`Join @kyne3301 to check out all kyne projects!`
 
-`I'am a file Renamer Bot!`
+<b>Please send me any Telegram file and reply to that file /rename New Name.extention</b>
 
-
-`Join` @kyne3301 `to check out all kyne Projects.`
-
-<b>`Please send me any Telegram file and reply to that file /rename New Name.extention`</b>
-
-`Click` /help `for more details..`"""
+Click /help for more details.."""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "<b>📌Create your own kyneRename Bot! </b>  /help for Details"
     DOWNLOAD_START = "📥Trying to download to kyne3301 server..."
-    UPLOAD_START = "📤Trying to upload back"
+    UPLOAD_START = "📤Trying to upload back to you..."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
     AFTER_SUCCESSFUL_UPLOAD_MSG = "**If you find my work useful join my channel @kyne3301.**"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
@@ -26,12 +21,12 @@ class Translation(object):
     CUSTOM_CAPTION_UL_FILE = " "
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
-    HELP_USER = """ `kyneRename Bot help!`
+    HELP_USER = """ kyneRename Bot help!
     
-`📌 Send me any Telegram File.`
-`📌 Reply to that message to /rename new name.extension.`
+📌 Send me any Telegram File.
+📌 Reply to that message to /rename new name.extension.`
    
-<b> `To Create Your Own kyne click on` </b> <a href="https://t.me/kyne3301">kyne3301</a>
+<b> To Create Your Own kyne click on </b> <a href="https://t.me/kyne3301">kyne3301</a>
 
 
 📢Support Channel @kyne3301"""
