d4be9b6dfede7b3e347e0041d8ea884d860fa3aa
OႦʂϙ <63944348+obsq@users.noreply.github.com>
2020-05-30 12:48:03 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -51,7 +51,7 @@ async def start(bot, update):
     TRChatBase(update.from_user.id, update.text, "/start")
     await bot.send_message(
         chat_id=update.chat.id,
-        text="`Hi there! I'am kyneFileManager Bot`"
+        text="Translation.START
         reply_to_message_id=update.message_id
     )
 
